bd7a806563c38575e6e7dea4f85ca91d4cedba3c
Sergio Martinez <cheko@acidbeat.net>
2023-12-14 03:48:35 -0500

    Base

--- WebApiTestLinux/Program.cs
+++ WebApiTestLinux/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,6 +37,9 @@ app.MapGet("/weatherforecast", () =>
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
+app.MapGet("/", () => Results.Text("Hello, world!")).WithName("Index")
+.WithOpenApi();
 
 app.Run();
 
